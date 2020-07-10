3cf66fa53c6400ec558ed2bacd4355ea8e54c27a
Ynda Jas <yndajas@gmail.com>
2020-07-10 12:02:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,11 +65,11 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season, data|
-    if season == :winter
-      data.values.flatten
-    end
+    season.each do |holiday, supplies|
+      if season == :winter
+        data.values.flatten
+      end
   end
-  
 end
 
 def all_supplies_in_holidays(holiday_hash)
