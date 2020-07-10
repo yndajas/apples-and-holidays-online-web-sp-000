ed0bf65159b599934f62f58997f6da1c4492bbd5
Ynda Jas <yndajas@gmail.com>
2020-07-10 12:19:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,12 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize}:"
     data.each do |holiday, supplies|
-      puts "#{holiday.to_s.split.capitalize!.join}: #{supplies.join(",")}"
+      holiday = holiday.to_s.split
+      holiday.each do |word|
+        word.capitalize!
+      end
+      holiday.join!
+      puts "#{holiday}: #{supplies.join(",")}"
     end
   end
 end
